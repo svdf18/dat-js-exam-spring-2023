176053a29946b2b250425b0b9c1fd56d15a0f83a
svdf18 <simonvandeurs@hotmail.com>
2023-06-10 16:15:19 +0200

    test

--- environment-01/script.js
+++ environment-01/script.js
@@ -1,1 +1,31 @@
 "use strict";
+
+let students = [];
+
+window.addEventListener("load", initApp);
+
+async function initApp() {
+  await getData();
+  console.log(students);
+
+  displayStudents();
+}
+
+async function getData() {
+  const response = await fetch ("students.json");
+  students = response.json();
+
+  return students
+}
+
+function displayStudents() {
+  document.querySelector("#students-list").innerHTML = "";
+
+  for (const student of students) {
+    document.querySelector("#students-list").insertAdjacentHTML(
+      "beforeend", `
+      <li>${student.name} - ${student.mail}, semester: ${student.semester}, enrolled: ${student.enrolled} </li>
+      `
+    )
+  }
+}
