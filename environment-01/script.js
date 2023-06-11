fbe5b03c24e09bb58c19776ded5c8f5f8fea5aa9
svdf18 <simonvandeurs@hotmail.com>
2023-06-11 16:10:26 +0200

    Update script.js

--- environment-01/script.js
+++ environment-01/script.js
@@ -1,1 +1,36 @@
 "use strict";
+
+let students = [];
+
+window.addEventListener("load", initApp);
+
+async function initApp() {
+  await getData();
+  console.log(students);
+
+  sortStudentsByName();
+  displayStudents();
+}
+
+async function getData() {
+  const response = await fetch("students.json");
+  students = await response.json();
+}
+
+function displayStudents() {
+  document.querySelector("#students-list").innerHTML = "";
+
+  for (const student of students) {
+    if (student.enrolled === true) {
+    document.querySelector("#students-list").insertAdjacentHTML(
+      "beforeend", `
+        <li>${student.name} - ${student.mail}, semester: ${student.semester}, enrolled: ${student.enrolled} </li>
+      `
+    );
+  };
+};
+}
+
+function sortStudentsByName() {
+  students.sort((a, b) => a.name.localeCompare(b.name));
+}
